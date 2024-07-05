1096ef5c03d374436c746a0434252bf149510008
Gooble <bivinbheemaiah@gmail.com>
2024-07-05 23:47:00 +0530

    Pass 3 tests in 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = Math.round((temp - 32) * (5/9) * 10)/10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = Math.round((temp * (9/5) + 32)*10)/10;
 };
 
 // Do not edit below this line
