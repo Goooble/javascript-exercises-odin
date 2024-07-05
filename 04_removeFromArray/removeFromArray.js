6130a129fb80d0f04a567ceccbaeaab3d6454310
Gooble <bivinbheemaiah@gmail.com>
2024-07-05 20:50:33 +0530

    Clean test 2 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,13 @@
 const removeFromArray = function(...args) {
     let indexToRemove = [];
     for(let i = 1; i < args.length;i++){
-        let j = -1;
-        do{
-            
-        indexToRemove.push(args[0].indexOf(args[i], j+1));
-        if(args[0].indexOf(args[i], j+1) === -1){
-            break;
-        }
-        j=args[0].indexOf(args[i], j+1);
-        }while(1);
+        indexToRemove.push(args[0].indexOf(args[i]));
+        args[0].indexOf(args[i]);
     }
     for(let i = 0; i < indexToRemove.length;i++){
         args[0].splice(indexToRemove[i], 1);
     }
-    
+    console.log(args[0]);
     return args[0];
 };
 
