970e93e9cc36adacf2370cdc472f0a88a65bdf63
Gooble <bivinbheemaiah@gmail.com>
2024-07-05 13:16:03 +0530

    Completed exercise 03
    was supposed to use builtin methods but didnt know
    so ended up doing stuff manually lol

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //split into array
+    let stringArray = [];
+    let reversedArray = [];
+    
+    stringArray = string.split("");
+    //read the array in the opposite direction
+    for(let i = stringArray.length-1; i >=0; i--){
+    //input the readings into a new array
+    reversedArray.push(stringArray[i]);
+    }
+    //printout the string after removing commas
+    let reversedString = '';
+    for(let i = 0; i < reversedArray.toString().length; i+=2){
+        reversedString += reversedArray.toString().slice(i,i+1);
+    }
+    return reversedString;
+   // o,l,l,e,h,
+   // 0123456789
+    
 };
 
 // Do not edit below this line
