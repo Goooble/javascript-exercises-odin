18ed627c9a187e0fe07a73500bd82b5e8a6f2026
Gooble <bivinbheemaiah@gmail.com>
2024-07-05 20:42:50 +0530

    messed up

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,19 +2,18 @@ const removeFromArray = function(...args) {
     let indexToRemove = [];
     for(let i = 1; i < args.length;i++){
         let j = -1;
-        do{
-            
-        indexToRemove.push(args[0].indexOf(args[i], j+1));
-        if(args[0].indexOf(args[i], j+1) === -1){
-            break;
+        while(args[0].indexOf(args[i], j+1) !== -1){
+            indexToRemove.push(args[0].indexOf(args[i], j+1));
+            j=args[0].indexOf(args[i], j+1);
         }
-        j=args[0].indexOf(args[i], j+1);
-        }while(1);
     }
+    let k = 0;
     for(let i = 0; i < indexToRemove.length;i++){
-        args[0].splice(indexToRemove[i], 1);
+        
+        args[0].splice(indexToRemove[i]-k, 1);
+        k++;
     }
-    
+    console.log(args[0]);
     return args[0];
 };
 
