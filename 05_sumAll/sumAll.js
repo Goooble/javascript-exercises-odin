ea7914b2d076596e4dad4c449995e9ad09be96d8
Gooble <bivinbheemaiah@gmail.com>
2024-07-05 23:03:19 +0530

    Pass test 3 in 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,17 @@
-const sumAll = function(start, end) {
-    let sum = 0;
-    for(;start <= end; start++){
-        sum +=start;
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (start < end) {
+    for (; start <= end; start++) {
+      sum += start;
     }
-    console.log(sum);
-    return sum
+  } 
+  else {
+    for (; end <= start; end++) {
+      sum += end;
+    }
+  }
+  console.log(sum);
+  return sum;
 };
 
 // Do not edit below this line
