7b8bdbee8f4406b065bf33e141b65e06732c0c40
Gooble <bivinbheemaiah@gmail.com>
2024-07-15 17:12:29 +0530

    Finish 12
    les gooo onto calculator project!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let d = new Date();
+  year = d.getFullYear();
 
+  people.sort((a, b) => {
+    let aPerson;
+    let bPerson;
+    if ("yearOfDeath" in a) {
+      aPerson = a.yearOfDeath - a.yearOfBirth;
+    } else {
+      aPerson = year - a.yearOfBirth;
+    }
+    if ("yearOfDeath" in b) {
+      bPerson = b.yearOfDeath - b.yearOfBirth;
+    } else {
+      bPerson = year - b.yearOfBirth;
+      console.log(bPerson);
+    }
+    return aPerson > bPerson ? 1 : -1;
+  });
+  console.log(year);
+  return people[people.length - 1];
 };
 
 // Do not edit below this line
