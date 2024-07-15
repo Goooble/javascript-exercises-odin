10a0f93931c3675f6b724e42be0f0d664ab92f4a
Gooble <bivinbheemaiah@gmail.com>
2024-07-15 16:23:26 +0530

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(pos) {
+    if(pos < 0) return 'OOPS';
+    zero = 0;
+    one = 1;
+    let sequence = [zero, one];
+    for(let i = 2; i <= +pos; i++){
+        sequence[i] = sequence[i-1] + sequence[i-2];
+    }
+    return sequence[pos];
+};  
 
 // Do not edit below this line
 module.exports = fibonacci;
